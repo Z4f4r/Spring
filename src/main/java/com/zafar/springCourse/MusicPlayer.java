ff3af6001abeb04f9422121bffd953560dd24584
Zafarjon <idk677799@gmail.com>
2024-07-08 06:52:36 +0300

    6

--- src/main/java/com/zafar/springCourse/MusicPlayer.java
+++ src/main/java/com/zafar/springCourse/MusicPlayer.java
@@ -2,8 +2,34 @@ package com.zafar.springCourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
@@ -11,4 +37,3 @@ public class MusicPlayer {
         System.out.println("Playing: " + music.getSong());
     }
 }
-```````

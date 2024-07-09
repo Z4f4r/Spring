aa9ec8f2bf25e6e19ce25a260c4e3a04d7091ee7
Zafarjon <idk677799@gmail.com>
2024-07-09 15:45:56 +0300

    12

--- src/main/java/com/zafar/springCourse/ClassicalMusic.java
+++ src/main/java/com/zafar/springCourse/ClassicalMusic.java
@@ -1,25 +1,22 @@
 package com.zafar.springCourse;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("singleton")
 public class ClassicalMusic implements Music {
-
-    private ClassicalMusic() {
-
-    }
-    public static ClassicalMusic getClassicalMusic() {
-        return new ClassicalMusic();
-    }
-
+    @PostConstruct
     public void doMyInit() {
         System.out.println("Doing my initialization");
     }
 
+    @PreDestroy
     public void doMyDestroy() {
         System.out.println("Doing my destruction");
     }
-
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/com/zafar/springCourse/TestSpring.java
+++ src/main/java/com/zafar/springCourse/TestSpring.java
@@ -7,9 +7,13 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+
 
         context.close();
     }
